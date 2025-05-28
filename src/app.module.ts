3514d6e55f788da68ec15350c0486ac723dfb75e
joaovitor-pires <joao.2023014293@alunocesul.com>
2025-05-27 22:16:21 -0300

    Adicionado providers da tabela Models no appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,13 @@ import { CreateProductController } from './create-product.controller';
 import { PrismaService } from './prisma.service';
 import { CreateProductService } from './create-product.service';
 import { ProductsRepository } from './products.repository';
+import { CreateModelController } from './create-model.controller';
+import { CreateModelService } from './create-model.service';
+import { ModelsRepository } from './models.repository';
 
 @Module({
   imports: [],
-  controllers: [CreateProductController],
-  providers: [PrismaService, CreateProductService, ProductsRepository],
+  controllers: [CreateProductController, CreateModelController],
+  providers: [PrismaService, CreateProductService, ProductsRepository, CreateModelService, ModelsRepository],
 })
 export class AppModule {}
