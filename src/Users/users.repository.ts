c743509f8fad74ca8290bcc6cc49ba118f31658b
joaovitor-pires <joao.2023014293@alunocesul.com>
2025-06-19 22:29:58 -0300

    Adicionado users-repository e create-user

---
+++ src/Users/users.repository.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma.service";
+import { Prisma } from "@prisma/client";
+
+@Injectable()
+export class UsersRepository {
+    constructor(private prisma: PrismaService) {}
+
+    async findByEmail(email: string): Promise<Prisma.UserUncheckedCreateInput | null> {
+        return this.prisma.user.findUnique({
+            where: {
+            email,
+            },
+        });
+    }
+
+    async create(data: Prisma.UserUncheckedCreateInput): Promise<void> {
+        await this.prisma.user.create({
+            data,
+        });
+    }
+}
